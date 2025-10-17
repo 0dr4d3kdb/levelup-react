4370a3f4be927371bfbda72b60aae5b515208c5f
Daniel <danielbeta97@gmail.com>
2025-10-16 22:56:49 -0300

    catálogo llenado y inicio de reseña

--- src/components/pages/Catalogo.jsx
+++ src/components/pages/Catalogo.jsx
@@ -1,22 +1,52 @@
 import React from 'react'
 import Producto1 from '../organisms/Producto1'
 import Filtros from '../organisms/Filtros'
+import Buscador from '../molecules/Buscador'
 
 export default function Catalogo() {
   return (
     <>
+      <Buscador/>
       <div id="seccion2">
         <Filtros/>
         <div id="productos">
           <Producto1
             code="1"
-            image="/images/carcassonne.jpg"
-            name="Carcassonne"
+            image="/images/catan.jpg"
+            name="Catan"
             description="Un clásico juego de estrategia donde los jugadores compiten por colonizar y
                 expandirse en la isla de Catan. Ideal para 3-4 jugadores y perfecto para noches de juego en
                 familia o con amigos."
             category="Juegos de mesa"
-            price="$39.990"/>
+            price="$29.990 clp"/>
+          <Producto1
+            code="2"
+            image="/images/carcassonne.jpg"
+            name="Carcassonne"
+            description="Un juego de colocación de fichas donde los jugadores construyen el paisaje
+                alrededor de la fortaleza medieval de Carcassonne. Ideal para 2-5 jugadores y fácil de
+                aprender."
+            category="Juegos de mesa"
+            price="$24.990 clp"/>
+          <Producto1
+            code="4"
+            image="/images/control-xbox.jpg"
+            name="Auriculares Gamer HyperX Cloud II"
+            description="Ofrece una experiencia de juego cómoda con
+                botones mapeables y una respuesta táctil mejorada. 
+                Compatible con consolas Xbox y PC."
+            category="Accesorios"
+            price="$59.990 clp"/>
+          <Producto1
+            code="3"
+            image="/images/audifonos.jpg"
+            name="Controlador Inalámbrico Xbox Series X"
+            description="Proporcionan un sonido envolvente de calidad con un
+                micrófono desmontable y almohadillas de espuma viscoelástica para mayor comodidad
+                durante largas sesiones de juego."
+            category="Accesorios"
+            price="$79.990 clp"/>
+
         </div>
       </div>
     </>
--- src/components/pages/Resena.jsx
+++ src/components/pages/Resena.jsx
@@ -2,6 +2,40 @@ import React from 'react'
 
 export default function Resena() {
   return (
-    <div>Resena</div>
+      <main class="resena-container">
+        <h2 class="resena-titulo">
+            Reseña: Controlador Inalámbrico Xbox Series X
+        </h2>
+
+        <div class="resena-contenido">
+
+            <div class="resena-imagen">
+                <img src="images/control-xbox.jpg" alt="control xbox" style={{width:"600px"}}/>
+            </div>
+
+            <div class="resena-detalles">
+                <p>
+                    El <strong>Control Inalámbrico Xbox Series X</strong> ofrece una experiencia de juego de
+                    última generación. Diseñado ergonómicamente, con texturas antideslizantes y gatillos mejorados,
+                    proporciona mayor precisión y comodidad en sesiones largas.
+                </p>
+
+                <h3>Características principales:</h3>
+                <ul>
+                    <li>Compatibilidad total con Xbox Series X|S, Xbox One y PC (Windows).</li>
+                    <li>Botones mapeables y cruceta híbrida mejorada.</li>
+                    <li>Conectividad inalámbrica de baja latencia.</li>
+                    <li>Texturizado en gatillos, bumpers y carcasa trasera.</li>
+                    <li>Puerto de 3.5 mm para auriculares con cable.</li>
+                </ul>
+
+                <p class="precio">
+                    $59.990 CLP
+                </p>
+
+                <button type="button" class="agregar">Agregar al carro</button>
+            </div>
+        </div>
+    </main>
   )
 }
