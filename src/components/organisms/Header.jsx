212dc2721cdca50de56646050058f114a5f8b271
Daniel <danielbeta97@gmail.com>
2025-10-22 21:54:19 -0300

    hola

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -5,17 +5,20 @@ export default function
     return (
         <header>
             <div id="logo">
-                <img src="https://raw.githubusercontent.com/0dr4d3kdb/proyectofullstack1/a998f004585f2377fc7ea88e2dae116bd8ee0fb2/Level-Up.png" alt="Level-Up" />
+                <a href="/">
+                    <img src="https://raw.githubusercontent.com/0dr4d3kdb/proyectofullstack1/a998f004585f2377fc7ea88e2dae116bd8ee0fb2/Level-Up.png" alt="Level-Up" />
+                </a>
             </div>
             <nav id="catalogonav">
                 <a href="/">Inicio</a>
                 <a href="/catalogo">Catalogo</a>
                 <a href="/aboutus">Nosotros</a>
-                <a href="/blog">Blog</a>
+                <a href="#">Blog</a>
             </nav>
             <div id="extra">
-                <a href="#">Carro</a>
-                <a href="login">Usuario</a>
+                <a href="carrito"> Carro 🛒 </a>
+                <a href="perfil">Usuario</a>
+                <a href='login' >Iniciar Sesión</a>
             </div>
         </header>
     )
